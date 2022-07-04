8c18905f775f8783a84c70bdb625b7f40f5f7324
ari2310 <muhammad23ari@gmail.com>
2022-07-04 08:29:15 +0700

    start with frontend

--- HandShop/Backend/index.js
+++ HandShop/Backend/index.js
@@ -3,6 +3,7 @@ import db from "./config/Database.js";
 import router from "./routes/index.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
     // contoh membuat database dengan syntax 
     // import Users from "./models/UserModel.js"; 
 dotenv.config();
@@ -17,6 +18,7 @@ try {
     console.error('Unable to connect to the database:', error);
 }
 
+app.use(cors({ credentials: true, origin:'http://localhost:3000'}))
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
